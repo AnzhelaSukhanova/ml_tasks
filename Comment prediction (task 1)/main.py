a214b705eccbe7b8930dcccda4d17b7f54142f8d
AnzhelaSukhanova <bidelya@gmail.com>
2021-10-11 11:54:46 +0300

    add code for local run and used part of dataset

---
+++ Comment prediction (task 1)/main.py
@@ -0,0 +1,132 @@
+import sys
+import math
+from tabulate import tabulate
+from random import randrange
+from copy import copy
+from collections import defaultdict
+from statistics import mean, variance
+
+import pandas as pd
+
+feature_num = 0
+fold_num = 5
+epoch_num = 80
+
+
+def normalize(train_data):
+    min_row = train_data.min()
+    max_row = train_data.max()
+
+    norm_data = pd.DataFrame(columns=range(feature_num))
+    for index, row in train_data.iterrows():
+        norm_data = norm_data.append((row - min_row)/(max_row - min_row),
+                                     ignore_index=True)
+    norm_data.iloc[:, -1] = train_data.iloc[:, -1]
+    return norm_data
+
+
+def predict(row, weights):
+    pred_res = weights[0]
+    for i in range(1, len(row) + 1):
+        if math.isnan(row[i - 1]):
+            row.iat[i - 1] = 0
+        pred_res += weights[i] * row[i - 1]
+    return pred_res
+
+
+def sgd(folds, ground_truth, test_ind):
+    weights = [0] * (feature_num + 1)
+    rows_num = folds[0].shape[0]
+
+    for i in range(fold_num):
+        if i != test_ind:
+            train_data = folds[i]
+            gt = ground_truth[i]
+            for j in range(1, epoch_num):
+                error_sum = 0
+                step = 1 / j
+                grad = defaultdict(int)
+
+                for k, row in train_data.iterrows():
+                    pred = predict(row, weights)
+                    error = pred - gt[k]
+                    error_sum += error ** 2
+                    grad[0] += error
+                    for l in range(1, feature_num):
+                        grad[l] += error * row[l - 1]
+
+                for k in range(feature_num):
+                    weights[k] = weights[k] - step * (2 / rows_num) * grad[k]
+
+                print('epoch=%d, step=%.3f, mse=%.3f' % (j, step, error_sum / rows_num))
+
+    return weights
+
+
+def get_folds(train_data):
+    folds = []
+    ground_truth = []
+    data_copy = copy(train_data)
+    fold_size = int(len(train_data) / fold_num)
+
+    for _ in range(fold_num):
+        fold = pd.DataFrame(index=range(fold_size), columns=range(feature_num + 1))
+        for i in range(fold_size):
+            index = randrange(len(data_copy))
+            row = data_copy.iloc[index]
+            data_copy.drop(index)
+            fold.iloc[i] = row
+
+        ground_truth.append(fold.iloc[:, -1])
+        folds.append(fold.iloc[:, :-1])
+    return folds, ground_truth
+
+
+def get_stats(fold, ground_truth, weights):
+    error_sum = 0
+    errors = []
+    for k, row in fold.iterrows():
+        pred = predict(row, weights)
+        error = pred - ground_truth[k]
+        error_sum += error ** 2
+        errors.append(error)
+
+    n = fold.shape[0]
+    rmse = math.sqrt(error_sum / n)
+    average = mean(errors)
+    D = variance(errors)
+    return rmse, average, D
+
+
+def main(argv):
+    global feature_num
+
+    variants_num = 2 if len(argv) < 2 else argv[1]
+
+    for i in range(1, variants_num):
+        print('\nVariant: ' + str(i))
+        train_data = pd.read_csv('Dataset/Training/Features_Variant_' +
+                                 str(i) + '.csv', header=None)
+
+        feature_num = train_data.shape[1] - 1
+        train_data = normalize(train_data)
+        print('Normalization: done')
+        folds, ground_truth = get_folds(train_data)
+        print('Cross validation: done')
+
+        stats = pd.DataFrame(index=range(5), columns=['F1', 'F2', 'F3', 'F4', 'F5', 'weights'])
+        for i in range(fold_num):
+            weights = sgd(folds, ground_truth, i)
+            stats.iloc[i, 5] = weights
+            for j in range(fold_num):
+                fold_stats = 'Train:\n' if j != i else 'Test:\n'
+                rmse, mean, D = get_stats(folds[j], ground_truth[j], weights)
+                fold_stats += 'RMSE: ' + str(rmse) + '\n'
+                fold_stats += 'Mean: ' + str(mean) + '\n'
+                fold_stats += 'Variance: ' + str(D)
+                stats.iloc[i, j] = fold_stats
+        print(tabulate(stats, headers='keys', tablefmt='psql'))
+
+
+if __name__ == '__main__':
+    main(sys.argv)
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# ml_tasks
+# Machine learning tasks
 Homework assignments for the course Intelligent Systems (SPbU, 7th semester)
+
+### Task 1
+Dataset: Facebook Comment Volume Dataset.  
+Predict how many comments a post will receive. The task involves the implementation of gradient descent and the calculation of metrics for evaluating the quality of the model.  
+Solution steps:  
+* normalization of feature values;  
+* cross-validation for five folds and linear regression training;  
+* RMSE calculation.  
+Local run: `python main.py <number of data variants>`  
+There is also a **sgd.ipynb**. 
